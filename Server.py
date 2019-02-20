e18887985d928b7378a64379ac620b04fad560b3
Adin Desaputhran <adindesaputhran@gmail.com>
2019-02-20 10:32:43 +0530

    Add files via upload

--- Server.py
+++ Server.py
@@ -1,25 +1,33 @@
-import socket # for socket 
-import sys  
-  
-try: 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    print "Socket successfully created"
-except socket.error as err: 
-    print "socket creation failed with error %s" %(err) 
-  
-# default port for socket 
-port = 80
-  
-try: 
-    host_ip = socket.gethostbyname('www.google.com') 
-except socket.gaierror: 
-  
-    # this means could not resolve the host 
-    print "there was an error resolving the host"
-    sys.exit() 
-  
-# connecting to the server 
-s.connect((host_ip, port)) 
-  
-print "the socket has successfully connected to google \ 
-on port == %s" %(host_ip)
+import socket
+
+# next create a socket object
+s = socket.socket()
+print("Socket successfully created")
+
+# reserve a port on your computer in our
+# case it is 12345 but it can be anything
+port = 12345
+
+# Next bind to the port
+# we have not typed any ip in the ip field
+# instead we have inputted an empty string
+# this makes the server listen to requests
+# coming from other computers on the network
+s.bind(('', port))
+print("socket binded to %s" % (port))
+
+# put the socket into listening mode
+s.listen(5)
+print("socket is listening")
+
+# a forever loop until we interrupt it or
+# an error occurs
+while True:
+    # Establish connection with client.
+    c, addr = s.accept()
+    print('Got connection from', addr)
+    p: str = "Thank you for connecting"
+    # send a thank you message to the client.
+    c.send(bytes(p,"utf-8"))
+
+    # Close the connection with the client
